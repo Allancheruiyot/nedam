cb46314e45bae419e6eed98a45f987ffcc477a24
kenkode <wangoken2@gmail.com>
2018-02-22 16:45:24 +0300

    nedam

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'nedam',
+			'database'  => 'payroll',
 			'username'  => 'root',
 			'password'  => 'mysql',
 			'port' => '3306',
